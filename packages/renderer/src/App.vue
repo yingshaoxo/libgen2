e6059dd10b2e9130ad36bb3b132315f85f597bde
yingshaoxo <yingshaoxo@gmail.com>
2022-02-18 17:22:08 +0800

    we made a simple UI, it is time to implement the function

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,57 +1,80 @@
-<script lang="ts" setup>
-import ReactiveCounter from '/@/components/ReactiveCounter.vue';
-import ReactiveHash from '/@/components/ReactiveHash.vue';
-import ElectronVersions from '/@/components/ElectronVersions.vue';</script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+let dict = reactive({
+  tempData: {
+    keywords: "",
+    tableData: [
+      {
+        author: '',
+        title: '',
+        year: '',
+        language: '',
+        size: '',
+        extension: ''
+      }
+    ]
+  },
+});
+</script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="../assets/logo.svg"
-    width="150"
-  >
+  <div class="LibraryGenesisTitle">Library Genesis 2</div>
 
-  <p>
-    For a guide and recipes on how to configure / customize this project,<br>
-    check out the
-    <a
-      href="https://github.com/cawa-93/vite-electron-builder"
-      target="_blank"
-    >vite-electron-builder documentation</a>.
-  </p>
+  <div class="searchRow">
+    <input v-model="dict.tempData.keywords" />
+    <button class="searchButton">Search!</button>
+  </div>
 
-  <fieldset>
-    <legend>Test Vue Reactivity</legend>
-    <reactive-counter />
-  </fieldset>
-
-  <fieldset>
-    <legend>Test Node.js API</legend>
-    <reactive-hash />
-  </fieldset>
-
-  <fieldset>
-    <legend>Environment</legend>
-    <electron-versions />
-  </fieldset>
-
-  <p>
-    Edit
-    <code>packages/renderer/src/App.vue</code> to test hot module replacement.
-  </p>
+  <div class="resultList">
+    <el-table :data="dict.tempData.tableData" style="width: 100%">
+      <el-table-column prop="author" label="Author" min-width="100" />
+      <el-table-column prop="title" label="Title" min-width="400" />
+      <el-table-column prop="year" label="Year" />
+      <el-table-column prop="language" label="Language" />
+      <el-table-column prop="size" label="Size" />
+      <el-table-column prop="extension" label="Extension" />
+    </el-table>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 60px auto;
-  max-width: 700px;
 }
-fieldset {
-  margin: 2rem;
-  padding: 1rem;
+
+.LibraryGenesisTitle {
+  font-family: Georgia, "Times New Roman", Times, serif;
+  line-height: 21px;
+  border-spacing: 1px 1px;
+  text-decoration: none;
+  color: #a00000;
+  font-size: 40px;
+  margin-top: 40px;
+}
+
+.searchRow {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+
+  margin-top: 50px;
+  padding-left: 20px;
+  input {
+    width: 300px;
+  }
+  .searchButton {
+    margin-left: 4px;
+  }
+}
+
+.resultList {
+  margin-top: 60px;
 }
 </style>
